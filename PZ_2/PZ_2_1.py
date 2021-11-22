d516a30960cb57de7e89ee4e2f1468353cdac932
Michael Wilds <mmmmmgge@gmail.com>
2021-11-22 23:48:27 +0300

    добавлены условия в начало

--- PZ_2/PZ_2_1.py
+++ PZ_2/PZ_2_1.py
@@ -1,4 +1,6 @@
-q = 0  # для ачивки
+# Скорость первого автомобиля V1, скорость второго V2, расстояние между
+# ними S. Определить расстояние между ними через T часов
+
 input1 = True
 input2 = True
 input3 = True
@@ -17,9 +19,6 @@ while input1:  # проверка условия
         v1 = int(input('введите скорость первой машины относительно оси x в км/ч \n'))  # ввод
     except ValueError:  # обработка исключения
         print('введите корректное значение\n')
-        q += 1  # для заработки ачивки
-        if q == 100:  # ачивка
-            print('achievement unlocked "навиженiй"\n')
     if type(v1) == int:  # проверка на инт
         input1 = False
 
@@ -31,9 +30,7 @@ while input2:  # тут всё то же самое
         # должен уточнить, что скорость движения навстречу относительно оси х будет отрицательной
     except ValueError:
         print('введите корректное значение\n')
-        q += 1
-        if q == 200:
-            print('achievement unlocked "всё в порядке?\n"')
+
     if type(v2) == int:
         input2 = False
 
@@ -43,9 +40,7 @@ while input3:  # ввод времени
         t = int(input('введите время в часах\n'))
     except ValueError:
         print('введите корректное значение\n')
-        q += 1
-        if q == 300:
-            print('achievement unlocked "стоит переосмыслить свою жизнь"\n')
+
     if type(t) == int:
         input3 = False
 
@@ -55,9 +50,6 @@ while input4:  # ввод расстояния
         s = int(input('введите исходное расстояние между машинами в км\n'))
     except ValueError:
         print('введите корректное значение')
-        q += 1
-        if q == 400:
-            print('achievement unlocked "эх, мне бы столько времени"\n')
     if type(t) == int:
         input4 = False
 
