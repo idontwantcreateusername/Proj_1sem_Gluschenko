cbac2df52ae54ebbc034469f5cb6bd648b0e1139
Michael Wilds <mmmmmgge@gmail.com>
2022-06-16 00:10:13 +0300

    141 complete

--- qw/rec1.py
+++ qw/rec1.py
@@ -1,84 +1,65 @@
-def cnt(p, res):  # сумма в обычных списках
-    res += p[0]
-    if len(p) > 1:
-        res = cnt(p[1:], res)
-    return res
+def cnt(p, res=0):  # сумма в обычных списках
+    return cnt(p[1:], res + p[0]) if len(p) else res
 
 
-def jnt(p, res):  # сумма во вложенных списках
+def jnt(p, res=0):  # сумма во вложенных списках
     res += cnt(p[0], 0) if type(p[0][0]) is int else jnt(p[0], 0)
-    if len(p) > 1:
-        res = jnt(p[1:], res)
-    return res
+    return jnt(p[1:], res) if len(p) > 1 else res
 
 
-def mnt(p, res):  # подсчёт отрицательных
-    res += p[0] / abs(p[0]) - 1
-    if len(p) > 1:
-        res = cnt(p[1:], res)
-    else:
-        res /= -2
-    return int(res)
+def mnt(p, res=0):  # подсчёт отрицательных
+    return int(mnt(p[1:], res + p[0] / abs(p[0]) - 1) if len(p) else res / -2)
 
 
-def fib(p, res):
+def fib(p, res=[1, 1]):  # возврат фиибоначи до определённого элемента
     if 2 < p:
         res.append(res[-1] + res[-2])
         res = fib(p - 1, res)
     return res
 
 
-def rev(p, res):
-    if p:
-        res = res * 10 + p % 10
-        res = rev(p // 10, res)
-    return res
+def rev(p, res=0):  # реверсирование числа
+    return rev(p // 10, res * 10 + p % 10) if p else res
 
 
-def dmf(res, p, d):
-    if p > 1:
-        res *= d
-        res = dmf(res, p - 1, d)
-    return res
+def dmf(res, p, d):  # возведение в степень
+    return dmf(res * d, p - 1, d) if p > 1 else res
 
 
-def mcf(p, res):  # сумма в обычных списках
-    res = p[0] if p[0] > res else res
-    if len(p) > 1:
-        res = mcf(p[1:], res)
-    return res
+def mcf(p, res=-999999):  # максимальный элемент в списках
+    return mcf(p[1:], p[0] if p[0] > res else res) if len(p) > 0 else res
 
 
-def dtb(p, res):  # сумма в обычных списках
-    res += p % 2 * 10
-    if p == 1:
-        res = cnt(p // 2, res)
-    return res
-
+def dtb(p, res=''):  # перевод в двоичную систему
+    return dtb(p // 2, str(p % 2) + str(res)) if p > 1 else int('1' + str(res))
 
 
 qw = [9, 9, 9, 9]
-print(cnt(qw, 0))
+print('сумма во обычных списках:', cnt(qw))
+
 qw = [[9, 9, 9, 9], [9, 9, 9, 9], [9, 9, 9, 9], [9, 9, 9, 9]]
-print(jnt(qw, 0))
+print('сумма во вложенных списках:', jnt(qw))
 
 qw = [[[9, 9, 9, 9], [9, 9, 9, 9], [9, 9, 9, 9], [9, 9, 9, 9]],
       [[9, 9, 9, 9], [9, 9, 9, 9], [9, 9, 9, 9], [9, 9, 9, 9]],
       [[9, 9, 9, 9], [9, 9, 9, 9], [9, 9, 9, 9], [9, 9, 9, 9]]]
-print(jnt(qw, 0))
+print('сумма во вложенных списках(трехмерный):', jnt(qw))
+
+qw = [-9, 9, -9, -9]
+print('подсчёт отрицательных:', mnt(qw))
 
 qw = 12
-print(fib(qw, [1, 1]))
+print('Фиббоначи:', fib(qw))
 
 qw = 234234
-print(rev(qw, 0))
+print('реверсирование числа:', rev(qw))
 
 qw = 3
 wq = 4
-print(dmf(qw, wq, qw))
+print('возведение в степень:', dmf(qw, wq, qw))
 
-qw = [9, 3, 13, 2]
-print(mcf(qw, 0))
+qw = [9, 3, 13, 25]
+print('максимальный элемент в списках:', mcf(qw))
 
-qw = 123
-print(dtb(qw))
+qw = 2022
+print('перевод в двоичную систему:', dtb(qw))
