c2103e34ddbdf904304e5028dced0b99283681f5
Michael Wilds <mmmmmgge@gmail.com>
2022-06-16 14:21:32 +0300

    отчёты

--- PZ_15/PZ_15_1.py
+++ PZ_15/PZ_15_1.py
@@ -1,7 +1,5 @@
 # В матрице элементы второго столбца возвести в квадрат
 # Вариант 5
-import numpy as np
-
-print('\n'.join(list(map(lambda x: ' '.join(list(map(lambda y: str(int(y) ** 2) if list(x).index(y) == 2 else str(y), x))),
-                         np.random.randint(0, 100, (int(input('введите количество строк: ')),
-                                                   int(input('введите количество столбцов: '))))))))
+print('\n'.join(list(map(lambda x: ' '.join(list(map(lambda y: str(int(y) ** 2) if x.index(y) == 1 else str(y), x)
+                                                 )), list(
+    map(lambda n: n.split(), input('введите матрицу разделяя строки" | "(в одну строку)').split('|')))))))
