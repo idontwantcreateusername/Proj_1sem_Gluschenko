fff84ae58d9ec396905435894e936081a3772036
Michael Wilds <mmmmmgge@gmail.com>
2022-06-16 15:23:54 +0300

    отчёты

--- PZ_15/PZ_15_1.py
+++ PZ_15/PZ_15_1.py
@@ -2,4 +2,4 @@
 # Вариант 5
 print('\n'.join(list(map(lambda x: ' '.join(list(map(lambda y: str(int(y) ** 2) if x.index(y) == 1 else str(y), x)
                                                  )), list(
-    map(lambda n: n.split(), input('введите матрицу разделяя строки" | "(в одну строку)').split('|')))))))
+    map(lambda n: n.split(), input('введите матрицу разделяя строки" | "(в одну строку)\n').split('|')))))))
